77d263eccf2b01d0e783824c705ea4c59d715b9f
Cedrus81 <erez.eitan.97@gmail.com>
2023-07-24 12:32:41 +0300

    expese list, and expense item

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,13 +12,14 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> expenses = dummyExpenses;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses List...'),
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses)),
         ],
       ),
     );
---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,18 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList(this.expenses, {super.key});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, idx) => ExpenseItem(expenses[idx]),
+    );
+  }
+}
